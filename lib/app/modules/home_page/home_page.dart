2d900e7502a98a554f1a0c62a2f8083d7290fc0f
vinicius <vinicius853o@gmail.com>
2022-05-16 21:12:16 -0300

    adjusting widgets on the homepage

--- lib/app/modules/home_page/home_page.dart
+++ lib/app/modules/home_page/home_page.dart
@@ -1,57 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_toe/app/controllers/home_page_controller.dart';
+import 'package:tic_tac_toe/app/modules/home_page/widgets/background_home_page.dart';
+import 'package:tic_tac_toe/app/modules/home_page/widgets/board_game.dart';
+import 'package:tic_tac_toe/app/utils/widgets/primary_button.dart';
 
 class HomePage extends GetView<HomePageController> {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: GetBuilder<HomePageController>(
-              id: 'board',
-              builder: (controller) {
-                return GridView.builder(
-                  padding: const EdgeInsets.all(10),
-                  itemCount: 9,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () => controller.pressButton(index),
-                      child: Container(
-                        color: controller.tiles[index].color,
-                        child: Center(
-                          child: Text(
-                            controller.tiles[index].symbol.toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 72,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
+    return BackgroundHomePage(
+      children: [
+        Expanded(
+          child: GetBuilder<HomePageController>(
+            id: 'board',
+            builder: (controller) {
+              return BoardGame(
+                controller: controller,
+              );
+            },
           ),
-          ElevatedButton(
-            onPressed: () => controller.resetBoard(),
-            child: Text("Resetar"),
-          ),
-        ],
-      ),
+        ),
+        PrimaryButton(
+          onPressed: controller.resetBoard,
+          child: const Text("Resetar"),
+        ),
+      ],
     );
   }
 }
